c261ae3737aaefc88960dce343906346fc25dcd4
sashanevirkovets <sashanevirkovets@gmail.com>
2019-03-28 17:32:31 +0200

    Create be skeleton

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+const router = express.Router();
+
+
+// support parsing of application/json type post data
+app.use(express.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(express.urlencoded({ extended: false }));
+
+require('./routes')(app);
+
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
