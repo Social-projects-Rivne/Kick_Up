cdcc03c0d8f74fba60ef9e940a3a4c24dd6b5b6a
Sasha Trach <razorm3power@gmail.com>
2019-03-29 14:52:26 +0200

    Add twitter bootstrap component. JS part throws error

--- client/src/components/AppHeader/AppHeader.js
+++ client/src/components/AppHeader/AppHeader.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
 
 const AppHeader = props => {
     return (
-        <h1 style={{textAlign: "center"}}>This is AppHeader</h1>
+        <div>
+            <h1 style={{textAlign: "center"}}>This is AppHeader</h1>
+            <Button variant="primary" size="lg" block>Test Button</Button>
+        </div>
     );
 }
 
