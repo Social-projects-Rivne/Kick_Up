2ec2f311ef47cb37d960aec224e8d47b5a113384
Dennis Radai <yodeco@gmail.com>
2019-03-30 20:26:12 +0200

    Add https://github.com/mdbootstrap/React-Bootstrap-with-Material-Design package

--- client/src/components/AppHeader/AppHeader.js
+++ client/src/components/AppHeader/AppHeader.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-
+import React from "react";
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
 const AppHeader = props => {
-    return (
-        <div>
-            <h1 style={{textAlign: "center"}}>This is AppHeader</h1>
-            <Button variant="primary" size="lg" block>Test Button</Button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>This is AppHeader</h1>
+      <MDBCol>
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage
+            className="img-fluid"
+            src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+            waves
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </MDBCardText>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </div>
+  );
+};
 
 export default AppHeader;
