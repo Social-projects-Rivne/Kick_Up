20843ff220cad8f424dc71924913d8b65a25f33e
Yulia Fursovych <fursovych@gmail.com>
2019-03-31 21:06:06 +0300

    Created AppHeader, Home, Footer JSX Add React Bootstrap

--- client/src/containers/PageContainer/PageContainer.js
+++ client/src/containers/PageContainer/PageContainer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { withRouter } from 'react-router-dom'
 
 import AppHeader from './../../components/AppHeader/AppHeader';
+import Footer from './../../components/Footer/Footer';
 
 const pageContainer = props => {
     return (
         <>
             <AppHeader />
             {props.children}
+            <Footer />
         </>
     );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
