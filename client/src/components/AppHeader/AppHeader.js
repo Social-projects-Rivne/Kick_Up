c916d164c9181c98464a1a54e50318733a0b1028
Yulia Fursovych <fursovych@gmail.com>
2019-04-06 01:49:29 +0300

    Rewrite Header/Footer/Home components to material ui
    Add new logo
    Create mixins for @media selectors (in functions.scss)
    Create variables.scss for storing scss variables

--- client/src/components/AppHeader/AppHeader.js
+++ client/src/components/AppHeader/AppHeader.js
@@ -1,49 +1,107 @@
 import React from 'react';
 
 import '../../styles/index.scss';
-
-import { Link } from "react-router-dom";
-import { Navbar,  FormControl, Form, Nav, InputGroup } from 'react-bootstrap';
-import { faSignInAlt, faUsers, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import logo from '../../assets/images/logo.png';
 
-const AppHeader = props => (
-        <Navbar collapseOnSelect expand="lg" className = 'header'>
-            <Navbar.Brand href="/" className="mobile-logo d-block d-lg-none">
-                <img className = 'logoImg' src={logo} alt="Logo" />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" className="toggle" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav inline className="header-content">
-                    <div className="search-row">
-                        <Navbar.Brand href="/" className="site-logo d-none d-lg-block">
-                            <img className = 'logoImg' src={logo} alt="Logo" />
-                        </Navbar.Brand>
-                        <Form inline className="search-form">
-                            <InputGroup>
-                                <FormControl className="search-field" placeholder="Search"/>
-                            </InputGroup>
-                        </Form>
-                    </div>
-                     <div className="align-center">
-                        <Link to="/events" className="fa-icon">
-                            <FontAwesomeIcon icon={faCalendarAlt}/>
-                        </Link>
+import { AppBar, Toolbar, IconButton, InputBase, Link } from '@material-ui/core';
+//You can find icon names here: https://jxnblk.com/rmdi/
+import { EventAvailable, SupervisorAccount, PersonAdd, Person, MoreVert, Search } from '@material-ui/icons';
+import { Link as RouterLink } from 'react-router-dom';
 
-                        <Link to="/rooms" className="fa-icon">
-                            <FontAwesomeIcon icon={faUsers} />
-                        </Link>
-                    </div>
-                    <div className="align-right">
-                        <Link to="/register" className="fa-icon">
-                            <FontAwesomeIcon icon={faSignInAlt} />
-                        </Link>
-                    </div>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
+class AppHeader extends React.Component {
+    state = {
+        mobileMenuOpened: false,
+    };
+
+    handleMobileMenuToggle = event => {
+        this.mobileMenuOpened = !this.mobileMenuOpened;
+        this.setState({ mobileMenuOpened: this.mobileMenuOpened });
+    };
+
+    render() {
+        const { mobileMenuOpened } = this.state;
+        const renderMobileMenu = (
+            <div className={"mobile-menu" + (mobileMenuOpened ? "" : " hidden")}>
+                <IconButton className="icon-details">
+                    <Link component={RouterLink} to="/events">
+                        <EventAvailable />
+                    </Link>
+                </IconButton>
+                <IconButton className="icon-details">
+                    <Link component={RouterLink} to="/rooms">
+                        <SupervisorAccount />
+                    </Link>
+                </IconButton>
+                <IconButton className="icon-details">
+                    <Link component={RouterLink} to="/sign-in">
+                        <Person />
+                    </Link>
+                </IconButton>
+                <IconButton className="icon-details">
+                    <Link component={RouterLink} to="/register">
+                        <PersonAdd />
+                    </Link>
+                </IconButton>
+            </div>
+        );
+
+        return (
+            <header className="root">
+                <AppBar position="static" className="header-bg">
+                    <Toolbar>
+                        <IconButton className="logo-hover">
+                            <Link component={RouterLink} to="/">
+                                <img className="logo-img" src={logo} alt="Logo" />
+                            </Link>
+                        </IconButton>
+                        <form className="search-form">
+                            <div className="search">
+                                <div className="search-icon">
+                                    <Search />
+                                </div>
+                                <InputBase className="search-root" name="query" />
+                            </div>
+                        </form>
+                        <div className="section-desktop">
+                            <IconButton className="icon-details">
+                                <Link component={RouterLink} to="/events">
+                                    <EventAvailable />
+                                </Link>
+                            </IconButton>
+                            <IconButton className="icon-details">
+                                <Link component={RouterLink} to="/rooms">
+                                    <SupervisorAccount />
+                                </Link>
+                            </IconButton>
+                        </div>
+                        <div className="grow" />
+                        <div className="section-desktop">
+                            <IconButton className="icon-details">
+                                <Link component={RouterLink} to="/sign-in">
+                                    <Person />
+                                </Link>
+                            </IconButton>
+                            <IconButton className="icon-details">
+                                <Link component={RouterLink} to="/register">
+                                    <PersonAdd />
+                                </Link>
+                            </IconButton>
+                        </div>
+                        <div className="section-mobile">
+                            <IconButton
+                                aria-haspopup="true"
+                                onClick={this.handleMobileMenuToggle}
+                                className="icon-details"
+                            >
+                                <MoreVert />
+                            </IconButton>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+                {renderMobileMenu}
+            </header>
+        );
+    }
+}
 
 export default AppHeader;
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 import '../../styles/index.scss';
-import { Col } from 'react-bootstrap';
+
+import { Link as RouterLink } from 'react-router-dom';
+import { Link } from "@material-ui/core";
 
 const footer = props => {
     return (
         <footer className="footer-content">
-            <Col className="privacy-policy">
-                <a href="#">Privacy Policy</a>
-            </Col>
-            <Col className="copyright">
-                &copy; {new Date().getFullYear()}{" "}Copyright: <a href="/">KickUp</a>
-            </Col>
+            <div className="privacy-policy">
+                <Link component={RouterLink} to="/privacypolicy">
+                    Privacy Policy
+                </Link>
+            </div>
+            <div className="copyright">
+                <Link component={RouterLink} to="/">
+                    &copy; {new Date().getFullYear()}{" "}Copyright: <a href="/">KickUp</a>
+                </Link>
+            </div>
         </footer>
     );
 };
