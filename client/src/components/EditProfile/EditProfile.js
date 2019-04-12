49ecfaf889bdc7fa9b08aff9cce8dbb3b7b8b51a
Sasha Trach <razorm3power@gmail.com>
2019-04-12 14:48:44 +0300

    Current progress

--- client/src/components/EditProfile/EditProfile.js
+++ client/src/components/EditProfile/EditProfile.js
@@ -1,33 +1,69 @@
 import React, { Component } from "react";
 
+import Swiper from 'react-id-swiper/lib/ReactIdSwiper.full';
+import { Pagination } from 'swiper/dist/js/swiper.esm';
+import "react-id-swiper/src/styles/scss/swiper.scss";
+
 import { 
-    Typography, 
-    Grid, 
-    Paper,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    ListItemAvatar,
-    ExpansionPanel,
-    ExpansionPanelSummary,
-    ExpansionPanelDetails,
-    Avatar,
-    Fab
+    Typography,
+    TextField
 } from '@material-ui/core';
+
+// Global constants;
+const tabletWidth = 768;
+const swiperParams = {
+    //direction: 'vertical',
+    modules: [ Pagination ],
+    slidesPerView: '1',
+    loop: false,
+    pagination: {
+      el: ".edit-profile__form-pagination",
+      type: 'progressbar',
+      clickable: true
+    },
+    centeredSlides: true,
+    autoHeight: true,
+    spaceBetween: 30,
+    rebuildOnUpdate: true,
+    shouldSwiperUpdate: true,
+    containerClass: '.edit-profile__form',
+    // Event listeners;
+    on: {}
+};
+
+
 
 class EditProfile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            activeStep: 0,
         };
-        //this.componentDidMount = this.componentDidMount.bind(this);
+        
+        //this.handleResize = this.handleResize.bind(this);
     }
     render() {
         return (
             <div className="edit-profile">
-                <h1>Here will go edit profile</h1>
+                <form action="#" className="edit-profile__form-wrapper">
+                    <Swiper {...swiperParams} >
+                        <div key={1} className="swiper-slide">
+
+                        <TextField
+                            select
+                            variant="outlined"
+                            label="With Select"
+                            //value={this.state.weightRange}
+                            //onChange={this.handleChange('weightRange')}
+                            >
+                            </TextField>
+
+                        </div>  
+                        <div key={2} className="swiper-slide">
+                            <h1>222</h1>
+                        </div>                        
+                    </Swiper>
+                </form>
             </div>
         )
     }
