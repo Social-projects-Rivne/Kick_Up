014a2cdd3aebef9ca35eb8c0b2e0cf1d515642d0
sasha <sashanevirkovets@gmail.com>
2019-06-30 13:35:54 +0300

    Feature add moderator actions

---
+++ server/routes/admin/admin.js
@@ -0,0 +1,70 @@
+const Router = require('koa-router');
+const router = new Router({ prefix: '/api/admin' });
+const { authenticated, checkRole } = require('../../middlewares');
+const {Room, Event, Complaint} = require('../../models');
+const constants = require('../../config/constants');
+const entity_types = Object.values(constants.rating.entity_types);
+const validate = require('../../services/Validator');
+const Mailer = require('./../../services/Mailer');
+
+const rule = {
+  types:{
+    entity_type: `required|string|in:${entity_types.join()}`,
+  }
+};
+
+const handler = {
+  async list(ctx) {
+    const rooms = await Room.where({is_banned:1}).fetchAll();
+    const events = await Event.where({is_banned:1}).fetchAll();
+    ctx.body = {
+      rooms,
+      events
+    }
+  },
+  async banned(ctx){
+    await validate(ctx.params, rule.types);
+    const { entity_id, is_banned } = ctx.request.body;
+    const { entity_type } = ctx.params; 
+    const model = entity_type === constants.rating.entity_types.room ? Room : Event;
+    const existModel = await model.where({ id:entity_id }).fetch({require:true})
+    await existModel.save({ is_banned }, { patch:true });
+    ctx.body = '';
+  },
+  async complaints(ctx){
+    await validate(ctx.query, {
+      page: 'numeric|min:1'
+    });
+  const { page } = ctx.query;
+    const complaints = await Complaint
+    .fetchPage({page, pageSize: constants.pageSize, withRelated: ['creator']});
+    ctx.body = {
+      complaints,
+      complaintCount: complaints.pagination.rowCount,
+      pageCount: complaints.pagination.pageCount
+    }
+  },
+  async resolvedComplaint(ctx){
+    await validate(ctx.params, rule.types);
+    const { user_id, resolved } = ctx.request.body;
+    const { entity_type, entity_id } = ctx.params; 
+    const model = entity_type === constants.rating.entity_types.room ? Room : Event;
+    const existModel = await model.where({ id:entity_id }).fetch({require:true})
+    if(existModel){
+      const complaint = await Complaint.where({user_id, entity_id,entity_type}).fetch({
+        withRelated:['creator']
+      },{require:true});
+      await complaint.save({ resolved }, { patch: true });
+      await Mailer.resolvedMessage(complaint.related('creator').get('email'))
+    }
+    ctx.body = '';
+  }
+};
+
+router.use(authenticated, checkRole);
+router.get('/list', handler.list);
+router.put('/:entity_type/banned', handler.banned);
+router.get('/complaints', handler.complaints);
+router.put('/:entity_type/:entity_id/complaint/resolved', handler.resolvedComplaint);
+
+module.exports = router.routes();
---
+++ server/routes/compaint/complaint.js
@@ -0,0 +1,35 @@
+const Router = require('koa-router');
+const router = new Router({ prefix: '/api/admin' });
+const { authenticated, checkRole } = require('../../middlewares');
+const {Room, Event, Complaint} = require('../../models');
+const constants = require('../../config/constants');
+const entity_types = Object.values(constants.rating.entity_types);
+const validate = require('../../services/Validator');
+
+const rule = {
+  types:{
+    entity_type: `required|string|in:${entity_types.join()}`,
+  }
+};
+
+const handler = {
+  async createComplains(ctx){ 
+    await validate(ctx.params, rule.types);
+    const { text, entity_id } = ctx.request.body;
+    const { user_id } = ctx.state;
+    const { entity_type } = ctx.params; 
+    const model = entity_type === constants.rating.entity_types.room ? Room : Event;
+    await model.where({ id:entity_id }).fetch({require:true})
+    const existComplaint = await Complaint.where({user_id, entity_id,entity_type}).fetch();
+    if(existComplaint) {
+      ctx.throwSingle('You have already sent a complaint, we are working on it, thanks')
+    }
+    await new Complaint({user_id, text, entity_type, entity_id}).save();
+    ctx.body = ''
+  }
+};
+
+router.use(authenticated);
+router.post('/:entity_type/complaint/create', handler.createComplains);
+
+module.exports = router.routes();
