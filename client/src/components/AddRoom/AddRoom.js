0326e773c444f5851b130f91b0dd6bb737d96d77
Denis Radai <yodeco@gmail.com>
2019-07-03 00:09:48 +0300

    Fixes detected by Igor, add room then add room again

--- client/src/components/AddRoom/AddRoom.js
+++ client/src/components/AddRoom/AddRoom.js
@@ -5,7 +5,8 @@ import {
     addNewRoom, 
     loadRoomCategories, 
     loadRoomTags,
-    editRoom 
+    editRoom,
+    saveRoomDetails
 } from '../../store/actions/rooms';
 import { enqueueSnackbar } from '../../store/actions/toast';
 
@@ -59,14 +60,14 @@ class AddRoom extends React.Component {
                 activeStep: prevState.activeStep + 1,
                 authUser: this.props.rooms[this.props.roomsAmount - 1].creator.id === this.props.user.id
             }));
-        // In case room was successfully updated, redirect to roon;
+        // In case room was successfully updated, redirect to room;
         } else if (
             this.state.lastRoomLoaded === this.props.roomsAmount &&
             this.props.rooms.length > 0 &&
             this.props.rooms[this.props.roomsAmount - 1].wasEdited
         ) {
             this.props.history.push({ pathname: "/room/" + this.state.roomId });
-            this.props.showToast("Congratulations! Room created!", messageType.SUCCESS);
+            this.props.showToast("Congratulations! Room created!", messageType.SUCCESS);      
         }
     }
     componentDidMount() {
@@ -395,6 +396,7 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     addRoom : data => dispatch(addNewRoom(data)),
     editRoom: (id, data) => dispatch(editRoom(id, data)),
+    saveRoomDetails: (id, data) => dispatch(saveRoomDetails(id, data)),
     loadCatogories: () => dispatch(loadRoomCategories()),
     loadTags: () => dispatch(loadRoomTags()),
     showToast: (message, variant) => dispatch(enqueueSnackbar({
